1ff44d97a6c42968b20ca02550e21adb136a6a15
theof9563 <114589745+theof9563@users.noreply.github.com>
2022-09-28 09:24:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Theo Fein"; // update the part BETWEEN the "quotes"
+let userName = "theof9563"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
